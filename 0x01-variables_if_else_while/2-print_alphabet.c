8abb62fbb0e58c1540b03b1682acc5bda8339518
Max-munene <munenemaxwell2@gmail.com>
2022-03-11 12:25:54 -0800

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - using the main print the alphabet
+ *
+ * Description: print the alphabet in lowercase.
+ *
+ * Return:0 (success)
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
