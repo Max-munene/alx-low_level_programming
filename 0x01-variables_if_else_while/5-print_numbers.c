93565a343b5538b5460d32928bbd5db0f4864ffc
Max-munene <munenemaxwell2@gmail.com>
2022-03-11 13:33:54 -0800

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - using the main print numbers
+ *
+ * Description: print numbers of base 10
+ *
+ * Return:0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (int i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	printf("\n");
+	return (0);
+}
