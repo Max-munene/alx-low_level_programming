a153b67bac49899a68a5e6d7dee6de33f7ee446f
Max-munene <munenemaxwell2@gmail.com>
2022-03-11 14:28:52 -0800

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - using the main print numbers
+ *
+ * Description: print numbers of base 10 with putchar
+ *
+ * Return:0 (success)
+ */
+int main(void)
+{
+	int d;
+
+	for (d = 0; d <= 9; d++)
+	{
+		putchar(d);
+	}
+	putchar('\n');
+	return (0);
+}
