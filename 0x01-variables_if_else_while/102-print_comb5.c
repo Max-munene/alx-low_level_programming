624a0339be668506f9700faeb36041bda402e0c9
Max-munene <munenemaxwell2@gmail.com>
2022-03-15 03:32:43 -0700

    2 digit combinations can be same

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+/**
+ * main - print all possible 2 digit comb
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = 48; j <= 57; j++)
+		{
+			putchar(i);
+			putchar(j);
+
+			if ((i <= 57) && (j <= 57))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
