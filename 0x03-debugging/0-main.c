bb08a76b6e899789ae68fe96c51b068ef7b250c2
Max-munene <munenemaxwell2@gmail.com>
2022-03-16 13:48:36 -0700

    Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - prints n positive, negative or zero
+ *
+ * Description: checks and prints outcomes of n as positive
+ * or negative or zero
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+	return (0);
+}
