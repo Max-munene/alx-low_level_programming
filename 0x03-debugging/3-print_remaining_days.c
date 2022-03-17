dda6771458f5ef5f292d525f66aeac5bc366150a
Max-munene <munenemaxwell2@gmail.com>
2022-03-17 03:49:13 -0700

    leap year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,34 +12,34 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	int months_cum_days[] = {31, 59, 90, 120, 151, 181, 212, 243, 273, 0, 0, 0};
+int months_cum_days[] = {31, 59, 90, 120, 151, 181, 212, 243, 273, 0, 0, 0};
 
-	months_cum_days[9] = 304;
-	months_cum_days[10] = 334;
-	months_cum_days[11] = 365;
+months_cum_days[9] = 304;
+months_cum_days[10] = 334;
+months_cum_days[11] = 365;
 
-    if ((year % 4 == 0 && year > 1582) && (year % 400 == 0 || year % 100 != 0))
-    {
-        if (month > 2)
-        {
-            day++;
-        }
+if ((year % 4 == 0 && year > 1582) && (year % 400 == 0 || year % 100 != 0))
+{
+if (month > 2)
+{
+day++;
+}
 
-        printf("Day of the year: %d\n", day);
-        printf("Remaining days: %d\n", 366 - day);
-    }
-    else
-    {
-        if (month >= 2 && day > months_cum_days[month - 1])
-        {
-  	int day_of_month = day - months_cum_days[month - 2];
+printf("Day of the year: %d\n", day);
+printf("Remaining days: %d\n", 366 - day);
+}
+else
+{
+if (month >= 2 && day > months_cum_days[month - 1])
+{
+int day_of_month = day - months_cum_days[month - 2];
 
-            printf("Invalid date: %02d/%02d/%04d\n", month, day_of_month, year);
-        }
-        else
-        {
-            printf("Day of the year: %d\n", day);
-            printf("Remaining days: %d\n", 365 - day);
-        }
-    }
+printf("Invalid date: %02d/%02d/%04d\n", month, day_of_month, year);
+}
+else
+{
+printf("Day of the year: %d\n", day);
+printf("Remaining days: %d\n", 365 - day);
+}
+}
 }
