e6468a47903517e939860bad6348eeab772243cc
Max-munene <munenemaxwell2@gmail.com>
2022-03-18 12:33:24 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,30 +7,34 @@
  */
 int main(void)
 {
-int i;
+	int i;
 	
-for (i = 1; i <= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
-	if(i % 3 == 0 && i % 5 == 0)
-	{
-	printf("FizzBuzz");
-}
-else if (i % 3 == 0)
-	{
-	printf("Fizz");
+		if(i % 3 == 0 && i % 5 == 0)
+		{
+		printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d",i);
+		}
+		if (i < 100)
+		{
+			putchar(' ');
+		}
+		else
+		{
+			putchar('\n');
+		}
 	}
-else if (i % 5 == 0)
-	{
-printf("Buzz");
-	}
-else
-	{
-		printf("%d",i);
-	}
-	if (i < 100)
-	putchar(' ');
-else
-		putchar('\n');
-}
-	return (0);
+		return (0);
 }
