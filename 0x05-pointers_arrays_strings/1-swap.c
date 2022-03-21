5b0ce658889fea72e079a72da965ea6f832048df
Max-munene <munenemaxwell2@gmail.com>
2022-03-21 10:18:39 -0700

    Swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * swap_int - function to swap values
+ * @a: pointer to be swapped
+ * @b: pointer to be swapped
+ *
+ */
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+	*b = *a;
+}
