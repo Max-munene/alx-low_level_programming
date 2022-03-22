960ad0980265838d95d86753cf860960a6b833b7
Max-munene <munenemaxwell2@gmail.com>
2022-03-22 12:32:18 +0300

    Reverse gear

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,16 +1,24 @@
 #include "main.h"
 
 /**
- * print_rev - Prints a string in reverse followed by a newline
- * @s: The string to be printed
+ * print_rev - print a string in reverse.
+ *
+ *@s: the string to print in reverse.
+ *
  */
+
 void print_rev(char *s)
 {
-	int length = 0;
+	int i = 0;
+	int length;
 
-	for (length = 0; s[length] != '\0';length--)
+	for (length = 0; s[length] != '\0'; length++)
 	{
-		_putchar(s[length]);
+	}
+
+	for (i = length - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
 	}
 	_putchar('\n');
 }
