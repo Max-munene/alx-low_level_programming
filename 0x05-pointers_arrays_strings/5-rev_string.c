91d629db4641f92d85d8f229fb8232f49509c591
Max-munene <munenemaxwell2@gmail.com>
2022-03-22 13:04:48 +0300

    Reverse String

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+void rev_string(char *s)
+{
+    int len, j, i;
+    char v1, v2;
+
+    for (len = 0; s[len] != '\0'; len++)
+    {
+
+    }
+
+        j = len - 1;
+        i = 0;
+
+    while (j > i)
+    {
+        v1 = s[i];
+        v2 = s[j];
+        s[i] = v2;
+        s[j] = v1;
+        j--;
+        i++;
+    }
+}
