11f6b003dad62633d8a2737421d16a3275694cc7
Max-munene <munenemaxwell2@gmail.com>
2022-03-24 15:08:56 -0700

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include<string.h>
+/**
+ * _strncpy - Concatenates two strings
+ * @dest: The destination string
+ * @src: The string copying the substring to append to dest
+ * @n: The maximum number of characters to copy from src string
+ *
+ * Return: The destination string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int dest_len = strlen(dest);
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[dest_len + i] = src[i];
+	}
+	dest[dest_len + i] = '\0';
+	return (dest);
+}
