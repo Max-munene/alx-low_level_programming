b97c8515fba74f7ef1942e729773886f2f571578
Max-munene <munenemaxwell2@gmail.com>
2022-03-24 06:56:09 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,10 +1,12 @@
 #include "main.h"
 #include <string.h>
 /**
- * _strncat - concatenates strings
- * @dest: String to be concatenate
- * @src: string to be concatenated
- * @n: bytes
+ * _strncat - Concatenates two strings
+ * @dest: The destination string
+ * @src: The string containing the substring to append to dest
+ * @n: The maximum number of characters to copy from src string
+ *
+ * Return: The destination string
  */
 char *_strncat(char *dest, char *src, int n)
 {
