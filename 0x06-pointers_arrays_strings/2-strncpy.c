79ef9afe69d6b7f1406c5b3f6f6a46de373b5e58
Max-munene <munenemaxwell2@gmail.com>
2022-03-24 15:15:53 -0700

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,13 +10,5 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int dest_len = strlen(dest);
-	int i;
-
-	for (i = 0; i < n && src[i] != '\0'; i++)
-	{
-		dest[dest_len + i] = src[i];
-	}
-	dest[dest_len + i] = '\0';
-	return (dest);
+	return (strncpy(dest,src,n));
 }
