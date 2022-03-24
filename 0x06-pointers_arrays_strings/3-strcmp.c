e47525cb0f3d0cfac1f035538f3ea1dce210591f
Max-munene <munenemaxwell2@gmail.com>
2022-03-24 15:25:33 -0700

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,12 @@
+#include<string.h>
+#include "main.h"
+/**
+ * _strcmp - compares strings
+ * @s1: string to be compared
+ * @s2: string to be compared
+ * Return: returninteger comparison
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
