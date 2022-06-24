9bc3eb53183664c52e2910f94579667f015816cb
Max-munene <munenemaxwell2@gmail.com>
2022-06-24 16:00:40 -0700

    add vals

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - calculates sum
+ * @argc: number of command-line arguments
+ * @argv: array of command-line arguments
+ *
+ * Return: 0 (Success), 1 Fail
+ *
+ */
+int main(int argc, char *argv[])
+{
+	int i, j, sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		j = 0;
+		while (argv[i][j] != '\0')
+		{
+			if (argv[i][j] >= '0' && argv[i][j] <= '9')
+			{
+				j++;
+			}
+			else
+			{
+				printf("Error");
+				return (1);
+			}
+		}
+
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+
+}
