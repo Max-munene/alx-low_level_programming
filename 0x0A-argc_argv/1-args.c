7ae9227159807265d8513c64d290cb27bad3439e
Max-munene <munenemaxwell2@gmail.com>
2022-06-24 13:52:09 -0700

    no. of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints number of arguments
+ *
+ * @argc: number of command-line arguments
+ * @argv: array of command-line arguments
+ *
+ * Return: (0) Always
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
